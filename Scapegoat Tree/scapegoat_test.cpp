ed1be9c19984ca4ce1a337b67a4058c3b79a5e8e
Tushar Harsora <tusharharsora95@gmail.com>
2021-02-10 21:59:41 +0530

    Modified some code for submission added Practical 3.docx

--- Scapegoat Tree/scapegoat_test.cpp
+++ Scapegoat Tree/scapegoat_test.cpp
@@ -6,8 +6,8 @@ int main() {
 	for (int i = 1; i <= 10; i++) {
 		tree.insert(i, i * 10);
 	}
-	std::cout << tree;
-	for (int i = 1; i < 15; i++) {
+	std::cout << tree << std::endl;
+	for (int i = 1; i <= 11; i++) {
 		tree.erase(i);
 		std::cout << "After Deleting : " << i << "\n" << tree << std::endl;
 	}
